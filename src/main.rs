1e5290ac681bb6a62d19dc95fa70271a2f8bf0a5
Jaehyeon Park <skystar@skystar.dev>
2022-10-04 14:50:16 +0900

    add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
+use clap::Parser;
+
 const DIC_URL: &str = "https://dic.daum.net/search.do";
 
 mod db;
 mod parse;
 
+#[derive(Parser, Debug)]
+struct Args {
+    /// search terms. multiple terms will be concatenated with space.
+    #[clap(required = true)]
+    words: Vec<String>,
+
+    /// remove the word from history
+    #[clap(long)]
+    forget: bool,
+
+    /// review the history
+    #[clap(long)]
+    review: bool,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
+    let args = Args::parse();
 
-    if args.len() == 1 {
-        println!("Usage: {} <word>", args[0]);
-        return Ok(());
+    if args.review {
+        unimplemented!("review feature is not implemented yet");
     }
 
-    let word = args[1..].join(" ");
+    if args.forget {
+        unimplemented!("forget feature is not implemented yet");
+    }
+
+    let word = args.words.join(" ");
 
     let raw_html = reqwest::get(format!("{}?q={}", DIC_URL, word))
         .await?
